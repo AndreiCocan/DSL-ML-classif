5b6e6c577e4a9132133e50331bd1d79967488227
Amelie Eveno <amelie.eveno@insa-rennes.fr>
2023-11-27 10:24:00 +0100

    ✨ first version of fuzzer

---
+++ NeoML/fuzzer.py
@@ -0,0 +1,88 @@
+import random
+import string
+
+# Define grammar elements
+scalers = ["MinMax", "Standard", "AbsMax"]
+kernels = ["linear", "poly", "rbf", "sigmoid", "precomputed"]
+criteria = ["gini", "entropy", "log_loss"]
+splitters = ["best", "random"]
+
+# Function to generate random strings
+def random_string():
+    return ''.join(random.choices(string.ascii_letters, k=random.randint(3, 8)))
+
+# Function to generate a valid Data block with end-of-line characters
+def generate_data():
+    data_block = f"data {random_string()} {{\n    source = '{random_string()}'"
+    if random.choice([True, False]):
+        data_block += f"\n    label = '{random_string()}'"
+    if random.choice([True, False]):
+        data_block += f"\n    drop = {' '.join([random_string() for _ in range(random.randint(1, 3))])}"
+    if random.choice([True, False]):
+        data_block += f"\n    scaler = {random.choice(scalers)}"
+    data_block += "\n}"
+    return data_block
+
+# Function to generate a valid Algo block with end-of-line characters
+def generate_algo():
+    algo_type = random.choice(["svm", "knn", "decisionTree", "mlp"])
+    algo_block = f"algo {random_string()} {algo_type} {{\n"
+    if algo_type == "svm":
+        algo_block += f"    C = {random.uniform(0, 1)}\n" if random.choice([True, False]) else ""
+        algo_block += f"    kernel = '{random.choice(kernels)}'\n" if random.choice([True, False]) else ""
+    elif algo_type == "knn":
+        algo_block += f"    n_neighbors = {random.randint(1, 10)}\n" if random.choice([True, False]) else ""
+        algo_block += f"    weights = '{random.choice(['uniform', 'distance'])}'\n" if random.choice([True, False]) else ""
+    elif algo_type == "decisionTree":
+        algo_block += f"    criterion = '{random.choice(criteria)}'\n" if random.choice([True, False]) else ""
+        algo_block += f"    splitter = '{random.choice(splitters)}'\n" if random.choice([True, False]) else ""
+        algo_block += f"    max_depth = {random.randint(1, 10)}\n" if random.choice([True, False]) else ""
+    elif algo_type == "mlp":
+        algo_block += f"    hidden_layer_sizes = {' '.join([str(random.randint(1, 10)) for _ in range(random.randint(1, 3))])}\n" if random.choice([True, False]) else ""
+    algo_block += "}"
+    return algo_block
+
+# Track generated data and algo block names
+generated_data_blocks = []
+generated_algo_blocks = []
+
+# Function to generate a valid Trainer block referencing existing Data and Algo blocks
+def generate_trainer():
+    if not generated_data_blocks or not generated_algo_blocks:
+        return ""
+
+    data_ref = random.choice(generated_data_blocks)
+    algo_ref = random.choice(generated_algo_blocks)
+
+    trainer_block = f"trainer {{\n    data = data.{data_ref}\n    model = algo.{algo_ref}"
+    trainer_block += f"\n    train_test_split = {random.uniform(0, 1)}" if random.choice([True, False]) else ""
+    trainer_block += f"\n    show_metrics = {'true' if random.choice([True, False]) else 'false'}"
+    trainer_block += "\n}"
+    return trainer_block
+
+# Function to generate a Model block with end-of-line characters
+def generate_model():
+    model_block = ""
+    for _ in range(random.randint(1, 10)):
+        choice = random.choice([generate_data, generate_algo, generate_trainer])
+        if choice == generate_data:
+            data_block = generate_data()
+            model_block += data_block + "\n\n"
+            # Extract data block name and add to the list of generated data blocks
+            data_name = data_block.split()[1]
+            generated_data_blocks.append(data_name)
+        elif choice == generate_algo:
+            algo_block = generate_algo()
+            model_block += algo_block + "\n\n"
+            # Extract algo block name and add to the list of generated algo blocks
+            algo_name = algo_block.split()[1]
+            generated_algo_blocks.append(algo_name)
+        else:
+            # Only generate a trainer if there are existing data and algo blocks
+            if generated_data_blocks and generated_algo_blocks:
+                model_block += generate_trainer() + "\n\n"
+    return model_block
+
+# Generate a program using Model entry
+generated_program = generate_model()
+print(generated_program)
