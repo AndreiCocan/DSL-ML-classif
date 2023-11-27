b0d6747a53e179dec8c8b558caa564ab0a5b6f9b
gselim <gmatis3lim@gmail.com>
2023-11-27 18:03:38 +0900

    compiled examples

---
+++ NeoML/tests/res-r/test1.r
@@ -0,0 +1,22 @@
+library(e1071) # SVM implementation
+library(class) # KNN implementation
+library(rpart) # DecisionTree implementation
+library(nnet) # MLP implementation
+library(caret)
+
+myData <- read.csv("tests/datasets/Iris.csv")
+
+myData_X <- subset(myData, select = -Species)
+myData_Y <- as.factor(myData$Species)
+
+preprocess_range <- preProcess(myData_X, method = "range")
+myData_X <- predict(preprocess_range, newdata = myData_X)
+
+train_index <- sample(1:nrow(myData), (1-0.5) * nrow(myData))
+myData_X_train <- myData_X[train_index, ]
+myData_X_test <- myData_X[-train_index, ]
+myData_Y_train <- myData_Y[train_index]
+myData_Y_test <- myData_Y[-train_index]
+
+myFirstModel <- svm(myData_X_train, myData_Y_train, kernel = "linear", cost = 1.0)
+
