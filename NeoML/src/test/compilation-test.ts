d7c4886394cdb5819b6325f96f987c6c22e99f29
gselim <gmatis3lim@gmail.com>
2023-12-05 18:37:46 +0100

    fin de compilation testing

--- NeoML/src/test/compilation-test.ts
+++ NeoML/src/test/compilation-test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect , test} from 'vitest';
 
 import { type Model} from '../language/generated/ast.js';
 
@@ -15,30 +15,50 @@ const services = createNeoMlServices(EmptyFileSystem).NeoMl;
 
 
 describe.each([
-  { neomlfile: '../Programs_examples/algo_block_decisionTree.neoml', quality:NaN, threshold: NaN },
-  { neomlfile: '../Programs_examples/algo_block_knn.neoml', quality:NaN, threshold: NaN },
-  { neomlfile: '../Programs_examples/algo_block_mlp.neoml', quality:NaN, threshold: NaN },
-  { neomlfile: '../Programs_examples/algo_block_svm.neoml', quality:NaN, threshold: NaN },
-  { neomlfile: '../Programs_examples/data_block_complete.neoml', quality:NaN, threshold: NaN },
-  { neomlfile: '../Programs_examples/data_block_simple.neoml', quality:NaN, threshold: NaN },
-  { neomlfile: '../Programs_examples/complete_minimalist_program.neoml', quality:1, threshold: 0.7 },
-  // { neomlfile: '../Programs_examples/complete_program_train_2_models.neoml', quality:1, threshold: 0.7 },
-   { neomlfile: '../Programs_examples/complete_program_with_unused_data_algo_blocks.neoml', quality:1, threshold: 0.7 },
-   { neomlfile: '../Programs_examples/complete_program.neoml', quality:1, threshold: 0.7 },
-   { neomlfile: '../Programs_examples/complete_test0.neoml', quality:1, threshold: 0.7 },
-   { neomlfile: '../Programs_examples/complete_test1.neoml', quality:1, threshold: 0.7 },
-   { neomlfile: '../Programs_examples/complete_test2.neoml', quality:0, threshold: 0.7 },
-])('compilation and execution of $neomlfile', async ({ neomlfile, quality,threshold }) => {
+  { neomlfile: '../Programs_examples/algo_block_decisionTree.neoml' },
+  { neomlfile: '../Programs_examples/algo_block_knn.neoml'},
+  { neomlfile: '../Programs_examples/algo_block_mlp.neoml'},
+  { neomlfile: '../Programs_examples/algo_block_svm.neoml'},
+  { neomlfile: '../Programs_examples/data_block_complete.neoml'},
+  { neomlfile: '../Programs_examples/data_block_simple.neoml' },
+  { neomlfile: '../Programs_examples/complete_minimalist_program.neoml' },
+  { neomlfile: '../Programs_examples/complete_program.neoml' },
+  { neomlfile: '../Programs_examples/complete_program_with_unused_data_algo_blocks.neoml'},
+  { neomlfile: '../Programs_examples/complete_program_train_2_models.neoml'},
+  { neomlfile: '../Programs_examples/complete_test0.neoml'},
+  { neomlfile: '../Programs_examples/complete_test1.neoml'},
+  { neomlfile: '../Programs_examples/complete_test2.neoml'},
+])('compilation and execution of $neomlfile', async ({ neomlfile}) => {
+
 
     const neomlScript = fs.readFileSync(neomlfile, 'utf-8');
 
     const model_python = await assertModelNoErrors(neomlScript);
 
-    const check_metrics:boolean=model_python.all_trainers.length>0 ? model_python.all_trainers.every(trainer =>trainer.show_metrics=='true'):false;
-
+    //const trainers_printing :boolean[]=model_python.all_trainers.length>0 ? model_python.all_trainers.map(trainer=>trainer.show_metrics=='true'):[];
     
+    //const check_metrics:boolean=model_python.all_trainers.length>0 ? model_python.all_trainers.every(trainer =>trainer.show_metrics=='true'):false;
+
+    // const data_ref_names=model_python.all_trainers.map(trainer=>trainer.data_ref.name)
+
+    // const data_names=data_ref_names.map(data_ref_name=>model_python.all_data.filter(data=>data.name==data_ref_name).findLast())
+    
+    // model_python.all_trainers.map(trainer=>trainer.data_ref.name).map(name=>model_python.all_data.filter(data=>data.name==name))
+
+    //const quality_list:boolean[] =model_python.all_data.filter(data=>model_python.all_trainers.map(trainer=>trainer.data_ref.name).includes(data.name)).map(data=>!data.source.includes("random"));
+  
     const fileNode_python = new CompositeGeneratorNode();
 
+    const classif_quality_list:boolean[]=model_python.all_trainers.map(
+      trainer=>
+        !model_python.all_data.filter(data=>data.name==trainer.data_ref.name)[0].source.includes("random")
+    );
+    const print_metrics_list:boolean[]=model_python.all_trainers.map(trainer=>trainer.show_metrics=='true');
+    const print_index:number[]=print_metrics_list.map((bool, index) => bool ? index : -1).filter(index => index !== -1);
+    const threshold=0.65;
+
+    
+  
     //python execution
     const generated_python_path =generateClassifierPython(model_python,'tmp.neoml','.',fileNode_python);
 
@@ -50,13 +70,13 @@ describe.each([
         else if (stderr) {
           console.warn(`stderr: ${stderr}`);
         }else{
-            console.log(neomlfile,'execution (metrics',check_metrics,') stdout',stdout);
+            //console.log(neomlfile,'execution (metrics',print_metrics_list,') (quality',classif_quality_list,') stdout',stdout);
+            console.log(neomlfile,'python execution stdout',stdout);
             resolve(stdout as string);
         }
         
       })
     });
-
   
     const model_r = await assertModelNoErrors(neomlScript);
 
@@ -72,51 +92,66 @@ describe.each([
         else if (stderr) {
           console.warn(`stderr: ${stderr}`);
         }else{
-            console.log(neomlfile,'execution (metrics',check_metrics,') stdout',stdout);
+            //console.log(neomlfile,'execution (metrics',print_metrics_list,') (quality',classif_quality_list,') stdout',stdout);
+            console.log(neomlfile,'r execution stdout',stdout);
             resolve(stdout as string);
         }
       })
     });
 
-    if (check_metrics && !Number.isNaN(quality) && !Number.isNaN(threshold)){
-      const python_accuracyValue=Number(result_python.split(":")[1]);
+    if (print_metrics_list.length>0 && print_metrics_list.some(metric_print=>metric_print)){
+      //si il y'a des trainers, et si un des trainers print bien un résultat
 
-        console.log("test1",python_accuracyValue);
-        test('checking python accuracy', () => { 
-          if(quality){
-            expect(threshold).toBeLessThan(python_accuracyValue);
-          }else{
-            expect(threshold).toBeGreaterThan(python_accuracyValue);
-          }
-  
-        });
-      
-      const r_accuracyValue = Number(result_r.match(/accuracy: (\d+\.?\d*)/)?.[1]);
+ // checking Python result
+    const python_accuracyValues = result_python.match(/Accuracy score : (\d+\.?\d*)/g)?.map(match => Number(match.split(":")[1])) || [];
 
-      console.log("test2",r_accuracyValue);
-      test('checking r accuracy', () => {
-        if (quality) {
-          expect(threshold).toBeLessThan(r_accuracyValue);
+    //console.log("python accuracies", python_accuracyValues);
+    test('checking python accuracy', () => {
+      print_index.forEach((quality_index,value_index) => {
+        if (classif_quality_list[quality_index]) {
+          expect(threshold).toBeLessThan(python_accuracyValues[value_index]);
         } else {
-          expect(threshold).toBeGreaterThan(r_accuracyValue);
+          expect(threshold).toBeGreaterThan(python_accuracyValues[value_index]);
         }
-
       });
+    });
 
-      console.log("test3",r_accuracyValue-python_accuracyValue);
-      test('comparing python and r results', () => {
-        expect(0.4).toBeGreaterThan(Math.abs(r_accuracyValue-python_accuracyValue));
+// checking R result
+    const r_accuracyValues = result_r.match(/accuracy: (\d+\.?\d*)/g)?.map(match => Number(match.split(":")[1])) || [];
+
+    //console.log("r accuracies", r_accuracyValues);
+    test('checking r accuracy', () => {
+      print_index.forEach((quality_index,value_index) => {
+        if (classif_quality_list[quality_index]) {
+          expect(threshold).toBeLessThan(r_accuracyValues[value_index]);
+        } else {
+          expect(threshold).toBeGreaterThan(r_accuracyValues[value_index]);
+        }
       });
+    });
+
+// comparing both results
+    //console.log("comparing both",r_accuracyValues.length,python_accuracyValues.length);
+    test('comparing python and r results', () => {
+      expect(r_accuracyValues.length).toBe(python_accuracyValues.length);
+      r_accuracyValues.forEach((r_accuracy,index)=>{
+          expect(0.4).toBeGreaterThan(Math.abs(r_accuracy-python_accuracyValues[index]));          
+      })
+      
+    });
 
     }else{
-      console.log("alternative r",result_r=='',"python",result_python=='');
+      //console.log("alternative r",result_r=='',"python",result_python=='');
       test('no output', () => {
         expect(result_r).toBe('');
         expect(result_python).toBe('');
       });
 
     }
-});
+
+    
+}
+);
 
 async function assertModelNoErrors(modelText: string) : Promise<Model> {
     var doc : LangiumDocument<AstNode> = await parseDocument(services, modelText)
